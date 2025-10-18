0de07c9beb13d3f3c44ceaca2cc824ac71b8f9e3
Nikita Gusev <gusevnikita09@gmail.com>
2025-10-18 19:09:33 +0300

    глава 1

---
+++ emptywindow.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+
+class EmptyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initializeUI()
+
+    def initializeUI(self):
+        self.setWindowTitle("Empty Window in PyQT")
+        self.setGeometry(200, 100, 800, 500)
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = EmptyWindow()
+    sys.exit(app.exec())
+
